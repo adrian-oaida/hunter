ff9d35fc1ee8d7a3983a0b1c05d48a47aea5feb1
Adrian oaida <adrian.horia@gmail.com>
2017-06-08 10:57:45 +0100

    feat(TP): modified run.sh to output graph pictures file name that are consistent with the ran program

--- TestPrograms/other/run.sh
+++ TestPrograms/other/run.sh
@@ -7,7 +7,10 @@ t=`timestamp`
 rm -f other_run
 rm -f temp_generated_*
 
+run_file=${1%".c"}
+
 gcc ../tools/*.c $1 -o other_run -lpthread -lm
-./other_run $2 $3 > temp_generated_$t
+./other_run ${@:2} > temp_generated_$t
 ../tools/makedotgraph.sh temp_generated_$t
-dot -Tpng temp_generated_$t.gv -o run_data.png > run_calls.png
+dot -Tpng temp_generated_$t.gv -o ${run_file}_data.png > ${run_file}_calls.png
+
--- TestPrograms/pipeline/run.sh
+++ TestPrograms/pipeline/run.sh
@@ -7,7 +7,9 @@ t=`timestamp`
 rm -f pipeline_run
 rm -f temp_generated_*
 
+run_file=${1%".c"}
+
 gcc ../tools/*.c $1 -o pipeline_run -lpthread -lm
-./pipeline_run $2 $3 > temp_generated_$t
+./pipeline_run ${@:2} > temp_generated_$t
 ../tools/makedotgraph.sh temp_generated_$t
-dot -Tpng temp_generated_$t.gv -o pipeline_run_data.png > pipeline_run_calls.png
+dot -Tpng temp_generated_$t.gv -o ${run_file}_data.png > ${run_file}_calls.png
