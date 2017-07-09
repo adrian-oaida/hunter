382255b7dc3392129b76db624d247f3cfd3ce061
Adrian oaida <adrian.horia@gmail.com>
2017-07-09 20:34:50 +0100

    feat(analyzer): lattice structure generator

--- analyzer/src/main/java/com/edin/hunter/main/Main.java
+++ analyzer/src/main/java/com/edin/hunter/main/Main.java
@@ -1,5 +1,7 @@
 package com.edin.hunter.main;
 
+
+import com.edin.hunter.matcher.LatticeGenerator;
 import com.edin.hunter.matcher.Matcher;
 import com.edin.hunter.runner.BaseRunner;
 import com.edin.hunter.runner.CRunner;
@@ -34,58 +36,41 @@ public class Main {
                 statefullBPipelineRunner = null,
                 produceConsumerRunner = null;
         try {
-//            statefullAPipelineRunner = new CRunner("/Users/dude/edin/msc/hunter/TestPrograms/pipeline/statefullA_pipeline.c", "/Users/dude/edin/msc/hunter/TestPrograms/tools/");
             statefullBPipelineRunner = new CRunner("/Users/dude/edin/msc/hunter/TestPrograms/pipeline/statefullC_pipeline.c", "/Users/dude/edin/msc/hunter/TestPrograms/tools/");
-//            simplePipelineRunner = new CRunner("/Users/dude/edin/msc/hunter/TestPrograms/pipeline/simple_pipeline.c", "/Users/dude/edin/msc/hunter/TestPrograms/tools/");
-//            r = new CRunner("/Users/dude/edin/msc/hunter/TestPrograms/sorting/serial_bublesort.c", "/Users/dude/edin/msc/hunter/TestPrograms/tools/");
-//        r.run("/Users/dude/edin/msc/hunter/TestPrograms/sorting/random_sort_input", "/Users/dude/edin/msc/hunter/TestPrograms/sorting/random_sort_output");
-
-//            produceConsumerRunner = new CRunner("/Users/dude/edin/msc/hunter/TestPrograms/other/producer_consumer.c", "/Users/dude/edin/msc/hunter/TestPrograms/tools/");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        simplePipelineRunner.run("5", "5");
-//        statefullAPipelineRunner.run("5", "5");
         statefullBPipelineRunner.run("3", "6");
-
-//        produceConsumerRunner.run("20", "");
-
-        Map<String, Graph> graphList = new HashMap<>();
-
         statefullBPipelineRunner.getDataFlowGraph().removeNode(0);
-
-
-//        graphList.put("Simple pipeline", simplePipelineRunner.getDataFlowGraph());
-//        graphList.put("Stateful A pipeline", statefullAPipelineRunner.getDataFlowGraph());
-        graphList.put("Stateful B pipeline", statefullBPipelineRunner.getDataFlowGraph());
-
-//        graphList.put("Simple pipeline call graph", simplePipelineRunner.getDynamicCallGraph());
-//        graphList.put("Stateful A pipeline call graph", statefullAPipelineRunner.getDynamicCallGraph());
-//        graphList.put("Stateful B pipeline call graph", statefullBPipelineRunner.getDynamicCallGraph());
-
-//        graphList.put("Producer Consumer Dataflow", produceConsumerRunner.getDataFlowGraph());
 //
-//        graphList.put("Producer Consumer Dynamic Call flow", produceConsumerRunner.getDynamicCallGraph());
-//        graphList.put("Producer Consumer Static Call flow", produceConsumerRunner.getStaticCallGraph());
+////        produceConsumerRunner.run("20", "");
+//
+//        Map<String, Graph> graphList = new HashMap<>();
+//
+//
+//        graphList.put("Stateful B pipeline", statefullBPipelineRunner.getDataFlowGraph());
 
 
-        AtomicInteger counter = new AtomicInteger(graphList.size());
+//        for(Map.Entry<String, Graph> g : graphList.entrySet()) {
+//
+//            Matcher matcher = new Matcher(g.getValue());
+//
 
-        int i = 0;
+        GraphViewer viewer = new GraphViewer(statefullBPipelineRunner.getDataFlowGraph());
+        viewer.periodicUpdateView(250);
+        viewer.displayGraph();
 
-        for(Map.Entry<String, Graph> g : graphList.entrySet()) {
-
-            Matcher matcher = new Matcher(g.getValue());
-
-            GraphViewer viewer = new GraphViewer(g.getValue());
-            viewer.displayGraph();
-
-//            matcher.detect();
-//            viewer.updateView();
-        }
+        Matcher matcher = new Matcher(statefullBPipelineRunner.getDataFlowGraph());
 
 
+        matcher.detect();
+
+
+
+        LatticeGenerator generator = new LatticeGenerator(3 , 6);
+        generator.generateLattice();
+        generator.displayLattice();
 
 
     }
---
+++ analyzer/src/main/java/com/edin/hunter/matcher/LatticeGenerator.java
@@ -0,0 +1,47 @@
+package com.edin.hunter.matcher;
+
+import com.edin.hunter.runner.Graph;
+import com.edin.hunter.runner.Node;
+import com.edin.hunter.ui.GraphViewer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dude on 7/9/17.
+ */
+public class LatticeGenerator {
+    private int workers;
+    private int stages;
+    private Graph graph;
+    public LatticeGenerator(int workers, int stages){
+        this.workers = workers;
+        this.stages = stages;
+        this.graph = new Graph("Lattice " + workers + " X " + stages + "");
+
+    }
+    public void generateLattice(){
+        int nodeCounter = 1;
+        Node[][] latticeStructure = new Node[workers][stages];
+
+        for(int i = 0; i < workers ; i++){
+
+            for(int j = 0; j < stages ; j++){
+                latticeStructure[i][j] = graph.getOrAddNode(nodeCounter++);
+                if(i > 0){
+                    latticeStructure[i - 1][j].addEdgeTo(latticeStructure[i][j]);
+                }
+                if(j > 0){
+                    latticeStructure[i][j - 1].addEdgeTo(latticeStructure[i][j]);
+                }
+            }
+        }
+
+
+    }
+    public void displayLattice(){
+        GraphViewer viewer = new GraphViewer(graph);
+
+        viewer.displayGraph();
+    }
+}
