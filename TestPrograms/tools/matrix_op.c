cbd604b22c51f148c0f1a1cd33b169b922e8d9c1
Adrian oaida <adrian.horia@gmail.com>
2017-07-10 15:39:59 +0100

    feat(TP/tools): added double matrix allocation

--- TestPrograms/tools/matrix_op.c
+++ TestPrograms/tools/matrix_op.c
@@ -2,7 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 
-int **alloc_matrix(int n, int m){
+double **alloc_double_matrix(int n, int m){
+    double **matrix = (double**) malloc(n * sizeof(double*));
+    for(int i=0; i < n; i++){
+        matrix[i] = (double*) malloc(m * sizeof(double));
+    }
+    return matrix;
+}
+
+int **alloc_int_matrix(int n, int m){
     int **matrix = (int**) malloc(n * sizeof(int*));
     for(int i=0; i < n; i++){
         matrix[i] = (int*) malloc(m * sizeof(int));
--- TestPrograms/tools/matrix_op.h
+++ TestPrograms/tools/matrix_op.h
@@ -1,7 +1,8 @@
 #ifndef MATRIX_OP_H
 #define MATRIX_OP_H
 
-int **alloc_matrix(int n, int m);
+double **alloc_double_matrix(int n, int m);
+int **alloc_int_matrix(int n, int m);
 void init_int_matrix(int **matrix, int n, int m);
 
 #endif
